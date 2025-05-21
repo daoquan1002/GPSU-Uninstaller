a33fd3a25d248859f6580160ce66d737a5b6b4f2
Quan. <100271192+daoquan1002@users.noreply.github.com>
2025-05-21 19:39:59 +0700

    Add files via upload

--- action.sh
+++ action.sh
@@ -1,12 +1,10 @@
 #!/system/bin/sh
-ui_print "*****************************************"
-ui_print "Google Play System Update Uninstaller"
-ui_print "by Quan."
-ui_print "*****************************************"
+echo [+] Google Play System Update Uninstaller
+echo [+] by Quan.
 sleep 5
-echo Uninstaller is begin
+echo - Uninstaller is begin
 sleep 5
-echo If you see FAILED is mean you have uninstalled before or it does not exist
+echo - If you see FAILED is mean you have uninstalled before or it does not exist
 sleep 5
 pm uninstall com.google.mainline.telemetry
 pm uninstall com.google.mainline.adservices
@@ -14,9 +12,9 @@ pm uninstall com.google.android.captiveportallogin
 pm uninstall com.google.android.modulemetadata
 pm uninstall com.google.android.networkstack
 pm uninstall com.google.android.documentsui
-echo Delete and freeze active folder
+echo - Delete and freeze active folder
 sleep 5
 rm -r /data/apex/active
 touch /data/apex/active
-echo Done! Reboot to take effect.
+echo - Done! Reboot to take effect.
 sleep 5
--- customize.sh
+++ customize.sh
@@ -4,9 +4,9 @@ ui_print "Google Play System Update Uninstaller"
 ui_print "by Quan."
 ui_print "*****************************************"
 sleep 5
-echo Uninstaller is begin
+echo - Uninstaller is begin
 sleep 5
-echo If you see FAILED is mean you have uninstalled before or it does not exist
+echo - If you see FAILED is mean you have uninstalled before or it does not exist
 sleep 5
 pm uninstall com.google.mainline.telemetry
 pm uninstall com.google.mainline.adservices
@@ -14,8 +14,8 @@ pm uninstall com.google.android.captiveportallogin
 pm uninstall com.google.android.modulemetadata
 pm uninstall com.google.android.networkstack
 pm uninstall com.google.android.documentsui
-echo Delete and freeze active folder
+echo - Delete and freeze active folder
 sleep 5
 rm -r /data/apex/active
 touch /data/apex/active
-echo Done! Reboot to take effect.
+echo - Done! Reboot to take effect.
